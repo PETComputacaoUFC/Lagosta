365d5c26d52ce3877c0b622918b72e5350346f8e
Ramos <gu.ramos@proton.me>
2025-04-19 02:05:34 -0300

    Added 3 item reading methods (random samples, circle, and square)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
+#include "reader.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <raylib.h>
 #include <raymath.h>
 #include <stdbool.h>
-#include <vector>
 
 #define CLAY_IMPLEMENTATION
 #include "../include/clay.h"
 #include "../include/clay_renderer_raylib.c"
 #include "filters.hpp"
+#include "reader.hpp"
+
+// base.png COORDS:
+// square:  63, 63  -->  1260, 869  |  (1197x806)
+// q01a:  293, 520  |  q11a:  631, 520
+// modalidade:  948, 520  |  fase:  977, 588
+// horizontal spacing: 57  |  vertical spacing: 34
+//
+// relative values:
+// q01a: 230, 457  |  q11a: 568, 457
+// modalidade: 885, 457  |  fase:  914, 525
+//
+// as percentages of the square (USE THESE VALUES IN Vector2Lerp):
+// q01a:  0.192147034, 0.566997519  |  q11a:  0.474519632, 0.566997519
+// modalidade:  0.7393448371, 0.566997519  |  fase:  0.763575606, 0.651364764
+// horizontal spacing: 0.04735  |  vertical spacing: 0.042
+float circle_radius = 9.0f;
+Vector2 circles[4] = {
+    { 63, 63 },
+    { 1260, 63 },
+    { 63, 869 },
+    { 1260, 869 },
+};
+
+const float Q01A_X = 0.192147034f, Q01A_Y = 0.566997519f;
+const float Q11A_X = 0.474519632f, Q11A_Y = 0.566997519f;
+const float MODALIDADE_X = 0.7393448371f, MODALIDADE_Y = 0.566997519f;
+const float FASE_X = 0.763575606f, FASE_Y = 0.651364764f;
+const float X_ITEM_SPACING = 0.04735f;
+const float Y_ITEM_SPACING = 0.042f;
 
 void HandleClayErrors(Clay_ErrorData errorData) {
     printf("%s", errorData.errorText.chars);
@@ -37,44 +67,19 @@ int main(void) {
     Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
 
     Image img_gabarito = LoadImage("resources/scans_teste_oci/out0002.png");
-    // ImageColorContrast(&img_gabarito, 100);
-    ImageThreshold(&img_gabarito, 210);
+    ImageThreshold(&img_gabarito, 220);
     ImageDilate(&img_gabarito, 5);
     ImageErode(&img_gabarito, 5);
-    // ImageInvert(&img_gabarito);
+
     Texture2D text_gabarito = LoadTextureFromImage(img_gabarito);
 
-    Camera2D camera = { };
+    Camera2D camera = {};
     camera.zoom = 0.75f;
 
-    // base.png COORDS:
-    // square:  63, 63  -->  1260, 869  |  (1197x806)
-    // q01a:  293, 520  |  q11a:  631, 520
-    // modalidade:  948, 520  |  fase:  977, 588
-    // horizontal spacing: 57  |  vertical spacing: 34
-    //
-    // relative values:
-    // q01a: 230, 457  |  q11a: 568, 457
-    // modalidade: 885, 457  |  fase:  914, 525
-    //
-    // as percentages of the square (USE THESE VALUES IN Vector2Lerp):
-    // q01a:  0.192147034, 0.566997519  |  q11a:  0.474519632, 0.566997519
-    // modalidade:  0.7393448371, 0.566997519  |  fase:  0.763575606, 0.651364764
-    // horizontal spacing: 0.04735  |  vertical spacing: 0.042
-    float circle_radius = 9.0f;
-    std::vector<Vector2> circles = {
-        { 63, 63 },
-        { 1260, 63 },
-        { 63, 869 },
-        { 1260, 869 },
-    };
-   
-    const float Q01A_X = 0.192147034f, Q01A_Y = 0.566997519f;
-    const float Q11A_X = 0.474519632f, Q11A_Y = 0.566997519f;
-    const float MODALIDADE_X = 0.7393448371f, MODALIDADE_Y = 0.566997519f;
-    const float FASE_X = 0.763575606f, FASE_Y = 0.651364764f;
-    const float X_ITEM_SPACING = 0.04735f;
-    const float Y_ITEM_SPACING = 0.042f;
+    Reader reader = Reader(&img_gabarito, circles, RANDOM_SAMPLE, 4);
+    for (int i = 0; i < 20; i++) {
+        reader.items[i] = Item();
+    }
 
     size_t dragging = -1;
     while (!WindowShouldClose()) {
@@ -86,7 +91,7 @@ int main(void) {
             Vector2 click_pos = GetScreenToWorld2D(GetMousePosition(), camera);
             if (dragging == (size_t) -1) {
                 size_t closest_circle = 0;
-                for (size_t i = 0; i < circles.size(); i++) {
+                for (size_t i = 0; i < 4; i++) {
                     Vector2 circle_center = circles[i];
                     if (Vector2Distance(circle_center, click_pos) <= Vector2Distance(circles[closest_circle], click_pos)) {
                         closest_circle = i;
@@ -100,11 +105,18 @@ int main(void) {
              dragging = -1;
         }
 
-        
+        // Prints box coords
         if (IsKeyPressed(KEY_P)) {
             printf("v0  x%.2f y%.2f | v1  x%.2f y%.2f\n", circles[0].x, circles[0].y, circles[1].x, circles[1].y);
             printf("v2  x%.2f y%.2f | v3  x%.2f y%.2f\n", circles[2].x, circles[2].y, circles[3].x, circles[3].y);
         }
+
+        // Updates reader and... well... reads, ig.
+        if (IsKeyPressed(KEY_R)) {
+            for (int i = 0; i < 4; i++) { reader.square[i] = circles[i]; }
+            reader.read();
+        }
+
 
         // ==== DRAW ====
         BeginDrawing();
@@ -115,43 +127,30 @@ int main(void) {
                     DrawCircleV(circle_center, circle_radius, RED);
                 }
 
-                for (int i = 0; i < 5; i++) {
-                    float l_x_lerp_amount = Q01A_X + X_ITEM_SPACING * (float) i;
-                    Vector2 l_v1 = Vector2Lerp(circles[0], circles[1], l_x_lerp_amount);
-                    Vector2 l_v2 = Vector2Lerp(circles[2], circles[3], l_x_lerp_amount);
-
-                    float r_x_lerp_amount = Q11A_X + X_ITEM_SPACING * (float) i;
-                    Vector2 r_v1 = Vector2Lerp(circles[0], circles[1], r_x_lerp_amount);
-                    Vector2 r_v2 = Vector2Lerp(circles[2], circles[3], r_x_lerp_amount);
-
-                    float m_x_lerp_amount = MODALIDADE_X + X_ITEM_SPACING * (float) i;
-                    Vector2 m_v1 = Vector2Lerp(circles[0], circles[1], m_x_lerp_amount);
-                    Vector2 m_v2 = Vector2Lerp(circles[2], circles[3], m_x_lerp_amount);
-
-                    float f_x_lerp_amount = FASE_X + X_ITEM_SPACING * (float) i;
-                    Vector2 f_v1 = Vector2Lerp(circles[0], circles[1], f_x_lerp_amount);
-                    Vector2 f_v2 = Vector2Lerp(circles[2], circles[3], f_x_lerp_amount);
-
-                    if (i < 3) {
-                        float y_lerp_amount = MODALIDADE_Y;
-                        Vector2 m_center = Vector2Lerp(m_v1, m_v2, y_lerp_amount);
-                        DrawCircleV(m_center, 5.0f, PURPLE);
-                    }
-
-                    if (i < 2) {
-                        float y_lerp_amount = FASE_Y;
-                        Vector2 f_center = Vector2Lerp(f_v1, f_v2, y_lerp_amount);
-                        DrawCircleV(f_center, 5.0f, PURPLE);
-                    }
-
-                    for (int i = 0; i < 10; i++) {
-                        float y_lerp_amount = Q01A_Y + Y_ITEM_SPACING * (float) i;
-                        Vector2 l_center = Vector2Lerp(l_v1, l_v2, y_lerp_amount);
-                        Vector2 r_center = Vector2Lerp(r_v1, r_v2, y_lerp_amount);
-                        DrawCircleV(l_center, 5.0f, PURPLE);
-                        DrawCircleV(r_center, 5.0f, PURPLE);
+                for (int i = 0; i < 10; i++) {
+                    float y_lerp_amount = Q01A_Y + Y_ITEM_SPACING * (float) i;
+                    for (int c = 0; c < 5; c++) {
+                        float x_lerp_amount = Q01A_X + X_ITEM_SPACING * (float) c;
+                        Vector2 v1 = Vector2Lerp(circles[0], circles[1], x_lerp_amount);
+                        Vector2 v2 = Vector2Lerp(circles[2], circles[3], x_lerp_amount);
+            
+                        Vector2 center = Vector2Lerp(v1, v2, y_lerp_amount);
+                        DrawCircleV(center, 5.0f, reader.items[i].choice == c ? ORANGE : PURPLE);
                     }
                 }
+
+                for (int i = 0; i < 10; i++) {
+                    float y_lerp_amount = Q11A_Y + Y_ITEM_SPACING * (float) i;
+                    for (int c = 0; c < 5; c++) {
+                        float x_lerp_amount = Q11A_X + X_ITEM_SPACING * (float) c;
+                        Vector2 v1 = Vector2Lerp(circles[0], circles[1], x_lerp_amount);
+                        Vector2 v2 = Vector2Lerp(circles[2], circles[3], x_lerp_amount);
+            
+                        Vector2 center = Vector2Lerp(v1, v2, y_lerp_amount);
+                        DrawCircleV(center, 5.0f, reader.items[i+10].choice == c ? ORANGE : PURPLE);
+                    }
+                }
+
             EndMode2D();
         EndDrawing();
     }
