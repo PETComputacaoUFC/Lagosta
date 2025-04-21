11e7c4c630d978e12e941adb7962904895429edd
Ramos <gu.ramos@proton.me>
2025-04-21 13:48:26 -0300

    added range struct

--- src/imgtools/align.cpp
+++ src/imgtools/align.cpp
@@ -33,3 +33,33 @@ Vector2 ImageFindTemplate(Image *image, Image *plate) {
 
     return { (float) match_x, (float) match_y };
 }
+
+struct Range {
+    float start;
+    float stop;
+    float step;
+    bool inclusive;
+
+    Range(float start, float end, float step = 1)
+        : start(start), stop(end), step(step), inclusive(false) {}
+    Range(float start, float end, float step, bool inclusive)
+        : start(start), stop(end), step(step), inclusive(inclusive) {}
+
+    struct Iterator {
+        float value;
+        float step;
+
+        Iterator(float value, float step) : value(value), step(step) {}
+        Iterator &operator++() { value += step; return *this; }
+        bool operator!=(const Iterator &other) const { return (value - other.value) * step < 0.0f; }
+        float operator*() const { return value; }
+    };
+
+    Iterator begin() const { return Iterator(start, step); }
+    Iterator end() const {
+        const float FLOAT_EPSILON = 4e-7; // erro estimado por float - número mágico ;P
+        float offset = -step * !inclusive; // offset dependendo se o range é inclusivo
+        float error = FLOAT_EPSILON * std::abs(stop - start) / step; // erro de float: epsilon * número de passos (nº de somas)
+        return Iterator(stop + offset + error, step);
+    }
+};
